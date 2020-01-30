970662ff281b3ddeea891c35d0c1aab9884aca05
Kysen Jackman <kysenj@gmail.com>
2020-01-29 23:24:56 -0700

    widget

--- src/App.js
+++ src/App.js
@@ -11,44 +11,11 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    // background: "rgb(36,36,36)",
     background: theme.palette.background,
     color: theme.palette.primary.light,
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-  },
-  gridContainer: {
-    alignItems: "center",
-    marginTop: 5,
-    width: "80vw",
-  },
-  paper: {
-    backgroundColor: "black",
-    color: theme.palette.primary.main,
-    height: "80vw",
-    cursor: "pointer",
-  },
-  thumbnail: {
-    height: "100%",
-    width: "100%",
-    objectFit: "cover",
-  },
-  descriptionWrapper: {
-    position: "absolute",
-    height: "80vw",
-    width: "80vw",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-
-    "&:hover": {
-      color: "red",
-    },
-  },
-  description: {
-    // display: "none"
   },
 }));
 
